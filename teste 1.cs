53a59c962834341c18d02f1e56d335674ba7e29d
marcelovinicius2 <marcelovinicius2@hotmail.com>
2021-08-05 22:07:56 -0300

    comite correção

--- teste 1.cs
+++ teste 1.cs
@@ -1,7 +1,7 @@
 using Internal;
-Console.WhiteLine('hello world');
-Console.WhiteLine('1');
-Console.WhiteLine('');
-Console.WhiteLine('2');
-Console.WhiteLine('');
-Console.WhiteLine('3');
+Console.WriteLine('hello world');
+Console.WriteLine('1');
+Console.WriteLine('');
+Console.WriteLine('2');
+Console.WriteLine('');
+Console.WriteLine('3');
